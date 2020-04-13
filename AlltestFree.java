5203bc1dcfa6091d179abb1a4121c758c5a7d9c0
Nikhilkranthi <43699795+Nikhilkranthi@users.noreply.github.com>
2020-04-13 16:22:00 +0530

    Update AlltestFree.java

--- AlltestFree.java
+++ AlltestFree.java
@@ -1,6 +1,13 @@
 package alltestfree;
-public class AllTestFree {
-    public static void main(String []args) throws InterruptedException
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class AllTestFree 
+{
+    public static void main(String []args) throws InterruptedException, IOException
      {
         String [][] arr=new String [20][20];
         for(int i=0;i<20;i++)
@@ -37,15 +44,40 @@ public class AllTestFree {
         arr[7][3]="*";
         arr[1][10]="*";
         int temp=1;
+        int d=0;
+        int len=1;
+        int bre=1;
         while(true)
         {
             if(temp==16)
                 break;
+            if(d==0)
+            {
             for(int i=temp;i<5+temp;i++)
             {
-                arr[1][i]="~";
+                arr[len][i]="~";
+                bre=i;
+                
             }
+            }
+            if(d==1)
+            {
+               for(int i=temp;i<5+temp;i++)
+                {
+                    arr[i][bre]="~";
+                    len=i;
+                } 
+            }
+//            if(d==3)
+//            {
+//               for(int i=temp;i<5+temp;i++)
+//                {
+//                    arr[i][bre]="~";
+//                    len=i;
+//                } 
+//            }
             temp++;
+            
         for(int i=0;i<20;i++)
         {
             for(int j=0;j<20;j++)
@@ -56,13 +88,38 @@ public class AllTestFree {
         }
          for(int i=1;i<19;i++)
             {
-                arr[1][i]=" ";
+                for(int j=1;j<19;j++)
+                {
+                     arr[i][j]=" ";
+                     arr[5][8]="*";
+                     arr[17][4]="*";
+                     arr[6][13]="*";
+                     arr[12][4]="*";
+                     arr[5][6]="*";
+                     arr[17][18]="*";
+                     arr[19][6]="*";
+                     arr[7][3]="*";
+                     arr[1][10]="*";
+                }
             }
-         Thread.sleep(500);
-         System.out.println();
+         Thread.sleep(550);
+       
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        long startTime = System.currentTimeMillis();
+        while ((System.currentTimeMillis() - startTime) < 500 && !in.ready()) 
+        {
         }
-        
-        
-             }
-    
+        if (in.ready()) 
+        {
+            Scanner s=new Scanner(System.in);
+            d=s.nextInt();
+        }
+
+         
+       }
+    }
+
 }
+    
+
